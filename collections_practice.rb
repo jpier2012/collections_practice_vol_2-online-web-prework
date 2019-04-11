79838bc72ef4f6c19f3f9c9bfe722b9b027291d8
James Pier <jpier2012@gmail.com>
2019-04-11 15:45:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 def begins_with_r(*args)
   all_r = true
-  
+
   args.each {|arg|
     if arg[0].to_s.downcase != "r"
       all_r = false
       break
     end
   }
-  
+
   all_r
 end
 
