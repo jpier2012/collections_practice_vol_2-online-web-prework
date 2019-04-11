d5b9e2591baaf3c0c98c4a5cdb5a1cf987a45479
James Pier <jpier2012@gmail.com>
2019-04-11 15:38:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,10 @@
 # your code goes here
 def begins_with_r(*args)
+  all_r = true
   args.each {|arg|
-    if arg.first.downcase == "r"
-      true
-    else
-      false
-      break
+    if arg.first.downcase != "r"
+      all_r = false
     end
   }
+  all_r
 end
